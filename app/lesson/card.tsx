da2ea8966baee8244da7535a97ec4209be8c67c7
Tetiana Kobryn <t.kobrin10@gmail.com>
2024-11-19 15:40:21 +0200

    Implemented challenge card

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -1,6 +1,6 @@
 import { challenges } from "@/db/schema";
 import { cn } from "@/lib/utils";
-import { boolean } from "drizzle-orm/mysql-core";
+import Image from "next/image";
 
 type Props = {
   id: number;
@@ -38,11 +38,46 @@ export const Card = ({
         selected &&
           status === "wrong" &&
           "border-rose-300 bg-rose-100 hover:bg-rose-100",
-        disabled && "",
+        disabled && "pointer-events-none hover:bg-white",
+        type === "ASSIST" && "w-full lg:p-3",
       )}
       onClick={() => {}}
     >
-      Card
+      {imageSrc && (
+        <div className="relative mb-4 aspect-square max-h-[80px] w-full lg:max-h-[150px]">
+          <Image src={imageSrc} fill alt={text} />
+        </div>
+      )}
+      <div
+        className={cn(
+          "flex items-center justify-between",
+          type === "ASSIST" && "flex-row-reverse",
+        )}
+      >
+        {type === "ASSIST" && <div />}
+        <p
+          className={cn(
+            "text-sm text-neutral-600 lg:text-base",
+            selected && "text-sky-500",
+            selected && status === "correct" && "text-green-500",
+            selected && status === "wrong" && "text-rose-500",
+          )}
+        >
+          {text}
+        </p>
+        <div
+          className={cn(
+            "flex h-[20px] w-[20px] items-center justify-center rounded-lg border-2 text-sm font-semibold text-neutral-500 lg:h-[30px] lg:w-[30px] lg:text-[15px]",
+            selected && "border-sky-300 text-sky-500",
+            selected &&
+              status === "correct" &&
+              "border-green-500 text-green-500",
+            selected && status === "wrong" && "border-rose-500 text-rose-500",
+          )}
+        >
+          {shortcut}
+        </div>
+      </div>
     </div>
   );
 };
