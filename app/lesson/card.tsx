b447f815b8c764ad230aca5395c843f3b0bf8dae
Tetiana Kobryn <t.kobrin10@gmail.com>
2024-11-19 16:05:55 +0200

    Implemented audio

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -1,6 +1,8 @@
 import { challenges } from "@/db/schema";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { useCallback } from "react";
+import { useAudio, useKey } from "react-use";
 
 type Props = {
   id: number;
@@ -27,6 +29,20 @@ export const Card = ({
   disabled,
   type,
 }: Props) => {
+  const [audio, _, controls] = useAudio({
+    src: audioSrc || "",
+  });
+
+  const handleClick = useCallback(() => {
+    if (disabled) return;
+
+    controls.play();
+
+    onClick();
+  }, [disabled, onClick, controls]);
+
+  useKey(shortcut, handleClick, {}, [handleClick]);
+
   return (
     <div
       className={cn(
@@ -41,8 +57,9 @@ export const Card = ({
         disabled && "pointer-events-none hover:bg-white",
         type === "ASSIST" && "w-full lg:p-3",
       )}
-      onClick={() => {}}
+      onClick={handleClick}
     >
+      {audio}
       {imageSrc && (
         <div className="relative mb-4 aspect-square max-h-[80px] w-full lg:max-h-[150px]">
           <Image src={imageSrc} fill alt={text} />
