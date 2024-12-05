5d2ea682693106202202e7f2e2d658654c5e1581
Tetiana Kobryn <t.kobrin10@gmail.com>
2024-12-05 21:58:29 +0200

    Added course creation

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import { Admin, ListGuesser, Resource } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import simpleRestProvider from "ra-data-simple-rest";
+import { CourseList } from "./course/list";
+import { CourseCreate } from "./course/create";
 
 const dataProvider = simpleRestProvider("/api");
 
 const AdminApp = () => {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource
-        name="courses"
-        list={ListGuesser}
-        recordRepresentation="title"
-      />
+      <Resource name="courses" list={CourseList} create={CourseCreate} recordRepresentation="title" />
     </Admin>
   );
 };
