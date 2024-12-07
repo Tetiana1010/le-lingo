7ecf84487f4e79d9b6262d3d0cf421bd8eb57060
Tetiana Kobryn <t.kobrin10@gmail.com>
2024-12-07 23:31:45 +0200

    Added edit api

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -4,13 +4,20 @@ import { Admin, Resource } from "react-admin";
 import simpleRestProvider from "ra-data-simple-rest";
 import { CourseList } from "./course/list";
 import { CourseCreate } from "./course/create";
+import { CourseEdit } from "./course/edit";
 
 const dataProvider = simpleRestProvider("/api");
 
 const AdminApp = () => {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="courses" list={CourseList} create={CourseCreate} recordRepresentation="title" />
+      <Resource
+        name="courses"
+        list={CourseList}
+        create={CourseCreate}
+        edit={CourseEdit}
+        recordRepresentation="title"
+      />
     </Admin>
   );
 };
