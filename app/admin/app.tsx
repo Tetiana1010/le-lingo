84e47238fb68c837e1852e1145933e1900e7daf3
Tetiana Kobryn <t.kobrin10@gmail.com>
2024-12-10 21:25:25 +0200

    Implemented challenge option admin controll

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -2,15 +2,25 @@
 
 import { Admin, Resource } from "react-admin";
 import simpleRestProvider from "ra-data-simple-rest";
+
 import { CourseList } from "./course/list";
 import { CourseCreate } from "./course/create";
 import { CourseEdit } from "./course/edit";
+
 import { UnitList } from "./unit/list";
 import { UnitCreate } from "./unit/create";
 import { UnitEdit } from "./unit/edit";
+
 import { LessonList } from "./lesson/list";
 import { LessonCreate } from "./lesson/create";
 import { LessonEdit } from "./lesson/edit";
+
+import { ChallengeList } from "./challenges/list";
+import { ChallengeCreate } from "./challenges/create";
+import { ChallengeEdit } from "./challenges/edit";
+import { ChallengeOptionList } from "./challengeOption/list";
+import { ChallengeOptionEdit } from "./challengeOption/edit";
+import { ChallengeOptionCreate } from "./challengeOption/create";
 
 const dataProvider = simpleRestProvider("/api");
 
@@ -37,6 +47,23 @@ const AdminApp = () => {
         create={LessonCreate}
         edit={LessonEdit}
         recordRepresentation="title"
+      />
+      <Resource
+        name="challenges"
+        list={ChallengeList}
+        create={ChallengeCreate}
+        edit={ChallengeEdit}
+        recordRepresentation="question"
+      />
+      <Resource
+        name="challengeOptions"
+        list={ChallengeOptionList}
+        create={ChallengeOptionCreate}
+        edit={ChallengeOptionEdit}
+        recordRepresentation="text"
+        options={{
+          label: "Challenge Options"
+        }}
       />
     </Admin>
   );
